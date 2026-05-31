7041c4dee510d1a48692d0d1f27e86181c0e2aeb
PromiseKidd <boaditheophilus34@gmail.com>
2026-05-31 10:18:19 +0000

    Add function to print initials in large letters
    Implemented a function to print initials in stylized letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Theophilus Boadi Appiagyei
+# STUDENT ID: 2425401020
+# Email: boaditheophilus34@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Theophilus Boadi Appiagyei, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large stylized letters
 
+    print()
+    print("...BAT")
+    print()
+
+    print("...BBBBBBBBBBBB........AAAAAAAAAAAA....TTTTTTTTTTTT...****")
+    print("...BB........BB........AA........AA........TTTT.......****")
+    print("...BB........BB........AA........AA........TTTT.......****")
+    print("...BB........BB........AA........AA........TTTT.......****")
+    print("...BBBBBBBBBBBB........AAAAAAAAAAAA........TTTT.......****")
+    print("...BB........BB........AA........AA........TTTT.......****")
+    print("...BB........BB........AA........AA........TTTT.......****")
+    print("...BB........BB........AA........AA........TTTT.......****")
+    print("...BB........BB...**...AA........AA...**...TTTT.......****")
+    print("...BBBBBBBBBBBB...**...AA........AA...**...TTTT.......****")
+
+    print()
 
 
 main()
